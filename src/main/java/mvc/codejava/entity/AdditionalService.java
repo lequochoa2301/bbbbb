7c40dcd94931afb6f75528671f5dbf8821f359ea
BichThuy-1901 <bachthibichthuy@gmail.com>
2024-10-14 19:16:53 +0700

    commit template

---
+++ src/main/java/mvc/codejava/entity/AdditionalService.java
@@ -0,0 +1,50 @@
+package mvc.codejava.entity;
+
+import javax.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class AdditionalService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String serviceName;
+    private double price;
+
+    @ManyToMany(mappedBy = "services")
+    private List<Booking> bookings;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+}
---
+++ src/main/java/mvc/codejava/repository/AdditionalServiceRepository.java
@@ -0,0 +1,8 @@
+package mvc.codejava.repository;
+
+
+import mvc.codejava.entity.AdditionalService;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdditionalServiceRepository extends JpaRepository<AdditionalService, Long> {
+}
